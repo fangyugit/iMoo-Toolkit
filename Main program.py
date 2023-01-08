502e96ef10232f427afb08e6d378fe9adf10c4a0
Ling1226 <wl_1226@foxmail.com>
2023-01-08 08:01:57 +0000

    update Main program.py.
    Signed-off-by: Ling1226 <wl_1226@foxmail.com>

--- Main program.py
+++ Main program.py
@@ -401,7 +401,7 @@ def mainp():
 
 
 if __name__ == "__main__":
-    print('此版本为canary版本，专门为测试使用，包含大量测试功能，极不稳定，请斟酌后使用\n此软件的任何版本皆免费（包括re版，高级版），不做也不允许做商业用途\n此项目由ReX 组织下ReX Wear 项目的 ReX iMoo Team 团队合力开发，版权及著作权属于ReX群组，请勿用其作商业用途，一经发现，将严肃处理！回车则默认\nQQ群158015384，论坛watch.rexwe.net')
+    print('此版本为canary版本，专门为测试使用，包含大量测试功能，极不稳定，请斟酌后使用\n此软件的任何版本皆免费（包括re版，高级版），不做也不允许做商业用途\n此项目由ReX 组织下ReX Wear 项目的 ReX iMoo Team 团队合力开发，版权及著作权属于ReX群组，请勿用其作商业用途，一经发现，将严肃处理！\nQQ群158015384，论坛watch.rexwe.net')
     accept=input('是否同意上述条款(y/n):')
     if accept == 'y':
 
