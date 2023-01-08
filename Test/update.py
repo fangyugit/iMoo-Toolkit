57ade19f8f08a3a1d99f7ab7848cd4e158776e34
Ling1226 <wl_1226@foxmail.com>
2023-01-08 11:32:37 +0000

    update Test/update.py.
    Signed-off-by: Ling1226 <wl_1226@foxmail.com>

--- Test/update.py
+++ Test/update.py
@@ -6,13 +6,6 @@ def file_creation(rout,name,format,msg):  # 路径，文件名，后缀名，内
     file.write(msg)
     file.close()
 
-def read(nam):  # 文件名（包括后缀）
-    file_object = open(nam, 'r')  # 文件对象
-    try:
-        all_the_text = file_object.read()  # 结果为str类型
-    finally:
-        file_object.close()
-
 list = os.getcwd()
 list=list.replace("\\", "\\\\")
 list=list+"\\\\"
@@ -20,7 +13,8 @@ list=list+"\\\\"
 file_creation(list,'mode','.dat','mode2')
 mode = os.path.exists('mode.dat')
 if mode == True:
-    print(mode)
-
-
-
+    file_object = open('mode.dat', 'r')  # 文件对象
+    try:
+        output=file_object.read()  # 结果为str类型
+    finally:
+        file_object.close()
